f2a3031784046083f14eadbd95a700a26fcfa310
The Natural <philip.smalls86@gmail.com>
2019-06-02 23:23:12 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   #your code here
 contacts.each do |person, data|
   data.each do |attribute, value|
-    if attribute == favorite_icecream_flavors
+    if attribute == :favorite_icecream_flavors
       value.each do |flavor|
         value.delete_if(flavor) == "strawberry"
        end
